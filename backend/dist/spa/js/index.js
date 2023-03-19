0162c79d64d9eb221f02763f57221275df7bb73f
Edilson da Sara <118057467+Ed-JackBoy-B7@users.noreply.github.com>
2023-03-19 01:45:29 -0700

    chore:netlify deploy

---
+++ backend/dist/spa/js/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/posts', (request, response) => {
+  let posts =[
+    {
+      caption: 'Golden Gate Bridge',
+      location: 'Maputo, Mozambique'
+    },
+    {
+      caption: 'Maputo Eye',
+      location:'Maputo'
+    }
+  ]
+  response.send(posts)
+
+})
+
+app.listen(process.env.PORT || 3005)
